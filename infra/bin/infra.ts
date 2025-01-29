da5069409569009247d4cf00e250e7da04f42fa5
citymaus <citymaus@users.noreply.github.com>
2025-01-28 21:46:34 -0800

    SKS - WEBSITE-48 - Working CDK

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,31 +1,50 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import * as fs from 'fs'
+import path = require('path');
 import { ApiStack } from '../stacks/api-stack';
 import { FrontendStack } from '../stacks/frontend-stack';
 import { DatabaseStack } from '../stacks/database-stack';
 import { AuthStack } from '../stacks/auth-stack';
 import { DnsStack } from '../stacks/dns-stack';
 import { CertificateStack } from '../stacks/certificate-stack';
+import { HostedZoneStack } from '../stacks/hostedzone-stack';
+import * as cdk from 'aws-cdk-lib';
+import * as fs from 'fs'
 
 const app = new cdk.App();
 
 //----------------------------------------------------------------
 // Set up environment configuration
 //----------------------------------------------------------------
-const env = app.node.tryGetContext('env') || process.env.ENV || 'dev';
-const configFile = `../env/${env}.json`;
+const env = app.node.tryGetContext('env'  ) || process.env.ENV || 'dev';
+const configFile = path.join(__dirname, `../env/${env}.json`);
 if (!fs.existsSync(configFile)) {
   throw new Error(`Configuration file ${configFile} not found`);
 }
 const config = require(`../env/${env}.json`);
+console.log(`Loaded configuration for ${env} environment.`);
 
-console.log(`Loaded configuration for ${env} environment:`, config);
+process.env.AWS_PROFILE = config.profile;
+console.log(`Using AWS profile: ${process.env.AWS_PROFILE}`);
+
+console.log('Deploying to Account:', config.account);
+console.log('Deploying to Region:', config.region);
+
+//----------------------------------------------------------------
+// Retrieve hosted zone (shared resource)
+//----------------------------------------------------------------
+const hostedZoneStackType = config.existingHostedZoneId ? "update" : "create";
+const hostedzoneStack = new HostedZoneStack(app, `HostedzoneStack-${hostedZoneStackType}-${env}`, { env: config });
 
 //----------------------------------------------------------------
 // Set up Route 53 hosted zone and ACM certificates
 //----------------------------------------------------------------
-const certificateStack = new CertificateStack(app, `CertificateStack-${env}`, { env: config });
+const certificateStackType = config.existingCertificateArn ? "update" : "create";
+const certificateStack = new CertificateStack(app, `CertificateStack-${certificateStackType}-${env}`, { env: config,
+  hostedZone: hostedzoneStack.hostedZone,
+  fullDomainName: hostedzoneStack.fullDomainName,
+  frontendUrl: hostedzoneStack.frontendUrl,
+  apiUrl: hostedzoneStack.apiUrl
+ });
 
 //----------------------------------------------------------------
 // Set up S3 and CloudFront
@@ -36,17 +55,6 @@ const frontendStack = new FrontendStack(app, `FrontendStack-${env}`, {
 });
 
 //----------------------------------------------------------------
-// Set up domain and DNS records
-//----------------------------------------------------------------
-new DnsStack(app, `DnsStack-${env}`, { 
-  env: config, 
-  certificate: certificateStack.certificate, 
-  fullDomainName: certificateStack.fullDomainName, 
-  hostedZone: certificateStack.hostedZone,
-  cloudFrontDistribution: frontendStack.cloudFrontDistribution
-});
-
-//----------------------------------------------------------------
 // Set up authorizer Lambda
 //----------------------------------------------------------------
 const authStack = new AuthStack(app, `AuthStack-${env}`, { env: config });
@@ -54,9 +62,23 @@ const authStack = new AuthStack(app, `AuthStack-${env}`, { env: config });
 //----------------------------------------------------------------
 // Set up API Gateway and Lambdas
 //----------------------------------------------------------------
-new ApiStack(app, `ApiStack-${env}`, { 
+const apiStack = new ApiStack(app, `ApiStack-${env}`, { 
   env: config, 
   httpLambdaAuthorizer: authStack.httpLambdaAuthorizer 
+});
+
+//----------------------------------------------------------------
+// Set up domain and DNS records
+//----------------------------------------------------------------
+new DnsStack(app, `DnsStack-${env}`, { 
+  env: config, 
+  certificate: certificateStack.certificate, 
+  fullDomainName: hostedzoneStack.fullDomainName, 
+  hostedZone: hostedzoneStack.hostedZone,
+  cloudFrontDistribution: frontendStack.cloudFrontDistribution,
+  frontendUrl: hostedzoneStack.frontendUrl,
+  apiUrl: hostedzoneStack.apiUrl,
+  apiGateway: apiStack.apiGateway
 });
 
 //----------------------------------------------------------------
--- infra/stacks/config/env-config.ts
+++ infra/stacks/config/env-config.ts
@@ -2,9 +2,14 @@ import * as cdk from 'aws-cdk-lib';
 
  export interface EnvStackProps extends cdk.StackProps {
     env: {
+        profile: string;
         account: string;
         region: string;
-        domainName: string;
+        delegateHostedNameServers: string[] | undefined;
         subDomainPrefix: string;
+        existingHostedZoneId: string | undefined;
+        existingCertificateArn: string | undefined;
+        existingDomainNameAliasApi: string | undefined;
+        existingDomainNameZoneId: string | undefined;
     };
  }
