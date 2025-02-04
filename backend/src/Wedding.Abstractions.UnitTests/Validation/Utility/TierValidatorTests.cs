7d0ca617e66f6e3119586e42fb13b034c11fd94a
citymaus <citymaus@users.noreply.github.com>
2025-02-03 20:35:34 -0800

    SKS - WEBSITE-75 - Added new enum values, fixing TierValidator

--- backend/src/Wedding.Abstractions.UnitTests/Validation/Utility/TierValidatorTests.cs
+++ backend/src/Wedding.Abstractions.UnitTests/Validation/Utility/TierValidatorTests.cs
@@ -1,4 +1,6 @@
-﻿using Wedding.Abstractions.Validation.Utility;
+﻿using FluentAssertions;
+using FluentValidation.TestHelper;
+using Wedding.Abstractions.Validation.Utility;
 using Wedding.Common.Utility.Testing.TestChain;
 
 namespace Wedding.Abstractions.UnitTests.Validation.Utility
@@ -7,10 +9,24 @@ namespace Wedding.Abstractions.UnitTests.Validation.Utility
     [UnitTestsFor(typeof(TierValidator))]
     public class TierValidatorTests
     {
-        [Test]
-        public void ShouldWriteTests()
+        private TierValidator _validator;
+
+        [SetUp]
+        public void Setup()
         {
-            Assert.Fail();
+            _validator = new TierValidator();
+        }
+
+        [TestCase("Platinum+")]
+        [TestCase("Platinum")]
+        [TestCase("Sapphire")]
+        [TestCase("Ruby")]
+        [TestCase("Amethyst")]
+        [TestCase("Opal")]
+        public void ShouldValidateGoodTiers(string tier)
+        {
+            var result = _validator.TestValidate(tier);
+            result.ShouldNotHaveAnyValidationErrors();
         }
     }
 }
