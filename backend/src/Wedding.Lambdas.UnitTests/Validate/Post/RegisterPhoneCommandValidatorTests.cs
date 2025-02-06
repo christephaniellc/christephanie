aae7814e20e63dcf3b0dc4da7491365039644691
citymaus <citymaus@users.noreply.github.com>
2025-02-05 23:52:05 -0800

    SKS - #WEBSITE-79 - WIP Initial commit for validate phone endpoint

---
+++ backend/src/Wedding.Lambdas.UnitTests/Validate/Post/RegisterPhoneCommandValidatorTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Wedding.Common.Utility.Testing.TestChain;
+using Wedding.Lambdas.Validate.Phone.Validation;
+
+namespace Wedding.Lambdas.UnitTests.Validate.Post
+{
+    [TestFixture]
+    [UnitTestsFor(typeof(RegisterPhoneCommandValidator))]
+    public class RegisterPhoneCommandValidatorTests
+    {
+        [Test]
+        public void ShouldWriteTests()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ backend/src/Wedding.Lambdas.Validate.Phone/Function.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.SimpleSystemsManagement.Model;
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Wedding.Abstractions.Enums;
+using Wedding.Common.DI;
+using Wedding.Common.Helpers.AWS;
+using Wedding.Common.Serialization;
+using Wedding.Lambdas.Validate.Phone.Commands;
+using Wedding.Lambdas.Validate.Phone.Handlers;
+using Wedding.Lambdas.Validate.Phone.Requests;
+using ValidationException = FluentValidation.ValidationException;
+
+namespace Wedding.Lambdas.Validate.Phone;
+
+public class Function
+{
+    private readonly ServiceProvider _serviceProvider;
+    private readonly IMapper _mapper;
+
+    public Function() : this(BuildDefaultServiceProvider())
+    {
+    }
+
+    public Function(ServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    private static ServiceProvider BuildDefaultServiceProvider()
+    {
+        var serviceCollection = new ServiceCollection();
+
+        serviceCollection.AddLambdaRegistrations(typeof(RegistrationHook));
+        serviceCollection.AddScoped<PhoneValidationHandler>();
+
+        return serviceCollection.BuildServiceProvider();
+    }
+
+    /// <summary>
+    /// Returns a well-formed USPS address
+    /// </summary>
+    /// <param name="request">The event for the Lambda function handler to process.</param>
+    /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
+    /// <returns></returns>
+    public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+    {
+        try
+        {
+            context.Logger.LogInformation($"Raw Input: {JsonSerializer.Serialize(request.Body)}");
+
+            var authContext = request.GetAuthContext();
+
+            using var scope = _serviceProvider.CreateScope();
+            var handler = scope.ServiceProvider.GetRequiredService<PhoneValidationHandler>();
+            
+            var phoneRequest = JsonSerializationHelper.DeserializeFromFrontend<ValidatePhoneRequest>(request.Body);
+            context.Logger.LogInformation($"Deserialized Input: {JsonSerializer.Serialize(phoneRequest)}");
+
+            if (phoneRequest.Action == VerifyEnum.Register)
+            {
+                var command = new RegisterPhoneCommand(authContext, phoneRequest.PhoneNumber);
+                var result = await handler.ExecuteAsync(command);
+                return result.OkResponse();
+            }
+
+            if (phoneRequest.Action == VerifyEnum.Validate)
+            {
+                var command = new ValidatePhoneCommand(authContext, phoneRequest.Code);
+                var result = await handler.ExecuteAsync(command);
+                return result.OkResponse();
+            }
+
+            // Otherwise, resend code
+            var resend = new ResendCodeCommand(authContext);
+            var resendResult = await handler.ExecuteAsync(resend);
+            return resendResult.OkResponse();
+        }
+        catch (TooManyUpdatesException ex)
+        {
+            var error = $"Too many requests exception: {ex.Message}";
+            context.Logger.LogError(error);
+
+            return error.ErrorResponse((int)HttpStatusCode.TooManyRequests, typeof(TooManyUpdatesException).ToString());
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            var error = $"Authorization exception: {ex.Message}";
+            context.Logger.LogError(error);
+
+            return error.ErrorResponse((int)HttpStatusCode.Unauthorized, typeof(UnauthorizedAccessException).ToString());
+        }
+        catch (ValidationException ex)
+        {
+            var error = $"Validation exception: {ex.Message}";
+            context.Logger.LogError(error);
+
+            return error.ErrorResponse((int)HttpStatusCode.BadRequest, typeof(ValidationException).ToString());
+        }
+        catch (Exception ex)
+        {
+            var error = $"Error occurred: {ex.Message}";
+            context.Logger.LogError(error);
+
+            return error.ErrorResponse((int)HttpStatusCode.InternalServerError, typeof(Exception).ToString());
+        }
+    }
+}
