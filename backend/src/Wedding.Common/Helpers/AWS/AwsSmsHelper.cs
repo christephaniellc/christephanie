441064ecfc12dd9ff18e2ee035649788be947b46
citymaus <citymaus@users.noreply.github.com>
2025-02-05 23:52:05 -0800

    SKS - #WEBSITE-79 - WIP Initial commit for validate phone endpoint

---
+++ backend/src/Wedding.Common/Helpers/AWS/AwsSmsHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+
+namespace Wedding.Common.Helpers.AWS
+{
+    public class AwsSmsHelper : IAwsSmsHelper
+    {
+        public async Task<HttpStatusCode?> SendVerificationCode(string phoneNumber, string message)
+        {
+            using (AmazonSimpleNotificationServiceClient snsClient = new AmazonSimpleNotificationServiceClient())
+            {
+                var snsRequest = new PublishRequest
+                {
+                    PhoneNumber = phoneNumber,
+                    Message = message
+                };
+
+                var result = await snsClient.PublishAsync(snsRequest);
+                return result.HttpStatusCode;
+            }
+        }
+    }
+}
---
+++ backend/src/Wedding.Lambdas.Validate.Phone/Validation/ResendCodeCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Wedding.Abstractions.Validation.Common;
+using Wedding.Abstractions.Validation.Utility;
+using Wedding.Lambdas.Validate.Phone.Commands;
+
+namespace Wedding.Lambdas.Validate.Phone.Validation
+{
+    /// <summary>
+    /// Validator for ResendCodeCommand.
+    /// Implements the <see cref="AbstractValidator{ ResendCodeCommand }" />
+    /// </summary>
+    /// <seealso cref="AbstractValidator{T}" />
+    internal class ResendCodeCommandValidator : AbstractValidator<ResendCodeCommand>, IValidate<ResendCodeCommand>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResendCodeCommand" /> class.
+        /// </summary>
+        public ResendCodeCommandValidator()
+        {
+            RuleFor(cmd => cmd.AuthContext)
+                .NotNull()
+                .SetValidator(new AuthContextValidator(false));
+        }
+
+        public void IsValid(ResendCodeCommand obj, object? _ = null)
+            => this.ValidateAndThrow(obj);
+    }
+}
