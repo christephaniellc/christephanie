183cfa38b030fb009b62984c8377d12b0aef4320
citymaus <citymaus@users.noreply.github.com>
2025-02-05 23:42:16 -0800

    SKS - #WEBSITE-76 - Adding initial database setup endpoint, requires knowledge of invitation code and first name

---
+++ backend/src/Wedding.Lambdas.Admin.Setup/Validation/AdminSetupCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using Wedding.Abstractions.Validation.Common;
+using Wedding.Abstractions.Validation.Utility;
+using Wedding.Lambdas.Admin.Setup.Commands;
+
+namespace Wedding.Lambdas.Admin.Setup.Validation
+{
+    /// <summary>
+    /// Validator for AdminSetupCommand.
+    /// Implements the <see cref="AbstractValidator{ AdminSetupCommand }" />
+    /// </summary>
+    /// <seealso cref="AbstractValidator{T}" />
+    internal class AdminSetupCommandValidator : AbstractValidator<AdminSetupCommand>, IValidate<AdminSetupCommand>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AdminSetupCommandValidator" /> class.
+        /// </summary>
+        public AdminSetupCommandValidator()
+        {
+            RuleFor(query => query.InvitationCode)
+                .SetValidator(new InvitationCodeValidator());
+        }
+
+        public void IsValid(AdminSetupCommand obj, object? _ = null)
+            => this.ValidateAndThrow(obj);
+    }
+}
