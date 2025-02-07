7c4d8e31465ffc3d400b29b488343f77bdef6ee7
citymaus <citymaus@users.noreply.github.com>
2025-02-06 21:18:30 -0800

    SKS - #WEBSITE-84 - WIP NotifyGuests

---
+++ backend/src/Wedding.Lambdas.Admin.NotifyGuests/Commands/VerifyPhoneNumberCommand.cs
@@ -0,0 +1,13 @@
+﻿using Wedding.Abstractions.Dtos.Auth;
+using Wedding.Common.Dispatchers;
+
+namespace Wedding.Lambdas.Admin.NotifyGuests.Commands
+{
+    /// <summary>
+    /// Class VerifyPhoneNumberCommand used to verify a phone number.
+    /// Implements the <see cref="IWeddingQuery" />
+    /// </summary>
+    /// <seealso cref="IWeddingQuery" />
+    /// <param name="PhoneNumber">Phone number to verify</param>
+    public record VerifyPhoneNumberCommand(AuthContext AuthContext, string PhoneNumber) : IWeddingQuery;
+}
