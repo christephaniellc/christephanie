1f21886eeeb5a114c1d70e7757f414f5969d8e1a
citymaus <citymaus@users.noreply.github.com>
2025-02-06 21:22:24 -0800

    SKS - #WEBSITE-84 - Removing WIP notifguests

--- backend/src/Wedding.Lambdas.Admin.NotifyGuests/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using Amazon.Lambda.Core;
-using System.Runtime.CompilerServices;
-using Wedding.Common.Serialization;
-
-[assembly: LambdaSerializer(typeof(CustomLambdaJsonSerializer))]
-[assembly: InternalsVisibleTo("Wedding.Lambdas.UnitTests")]
-[assembly: InternalsVisibleTo("Wedding.PublicApi.Logic")]
