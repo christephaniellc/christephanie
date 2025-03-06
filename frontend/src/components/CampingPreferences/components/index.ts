138dcde7a49b27b93093af97c6f60d896eef005f
Topher Sikorra <topher.sikorra@gmail.com>
2025-03-05 18:12:10 -0800

    build fix

--- frontend/src/components/CampingPreferences/components/index.ts
+++ frontend/src/components/CampingPreferences/components/index.ts
@@ -1,5 +1,2 @@
 export { default as PreferenceButtonGroup } from './PreferenceButtonGroup';
 export { default as PreferenceDescription } from './PreferenceDescription';
-export { default as HotelList } from './HotelList';
-export { default as HotelOption } from './HotelOption';
-export { default as HotelDetail } from './HotelDetail';
