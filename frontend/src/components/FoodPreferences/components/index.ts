d3ebf04e31a58ae43d6dbb741b86a46e49c3ae85
Topher Sikorra <topher.sikorra@gmail.com>
2025-03-08 17:01:06 -0800

    allergiesPage fiexed -- just the page not the modal

---
+++ frontend/src/components/FoodPreferences/components/index.ts
@@ -0,0 +1,5 @@
+export { default as AllergyButton } from './AllergyButton';
+export { default as AllergyButtonGroup } from './AllergyButtonGroup';
+export { default as AllergySelectionModal } from './AllergySelectionModal';
+export { default as BabyFoodInfo } from './BabyFoodInfo';
+export { default as ActiveEatingIcon } from './ActiveEatingIcon';
