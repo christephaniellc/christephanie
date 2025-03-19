af1e177a0ca608e32b73e5717b4825852f312484
Topher Sikorra <topher.sikorra@gmail.com>
2025-03-18 22:14:05 -0700

    rsvp card

--- frontend/src/routes/index.ts
+++ frontend/src/routes/index.ts
@@ -2,6 +2,7 @@ import AddTaskIcon from '@mui/icons-material/AddTask';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import HomeIcon from '@mui/icons-material/Home';
 import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import PrintIcon from '@mui/icons-material/Print';
 
 import asyncComponentLoader from '@/utils/loader';
 
@@ -42,6 +43,12 @@ const routes: Routes = {
     component: asyncComponentLoader(() => import('@/pages/VerifyEmail')),
     path: '/verify-email',
     title: 'Verify Email',
+  },
+  [Pages.PrintedRsvp]: {
+    component: asyncComponentLoader(() => import('@/pages/PrintedRsvp')),
+    path: '/printed-rsvp',
+    title: 'Printed RSVP',
+    icon: PrintIcon,
   },
   [Pages.NotFound]: {
     component: asyncComponentLoader(() => import('@/pages/NotFound')),
