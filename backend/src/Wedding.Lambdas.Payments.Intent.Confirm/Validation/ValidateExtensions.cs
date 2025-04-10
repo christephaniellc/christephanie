e956f9ea11d2fc95bd5b0cd6abf29861e8581c9f
citymaus <citymaus@users.noreply.github.com>
2025-04-09 20:29:17 -0700

    SKS - Adding new unauth endpoint for stripe webhook

---
+++ backend/src/Wedding.Lambdas.Payments.Intent.Confirm/Validation/ValidateExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Wedding.Common.Helpers;
+using Wedding.Lambdas.Payments.Intent.Confirm.Commands;
+
+namespace Wedding.Lambdas.Payments.Intent.Confirm.Validation
+{
+    public static class ValidateExtensions
+    {
+
+        /// <summary>
+        /// Validates the specified command.
+        /// </summary>
+        /// <param name="obj">The command.</param>
+        /// <param name="context">The context.</param>
+        public static void Validate(
+            this GetPaymentIntentStatusQuery obj,
+            object? context = default)
+            => ValidateHelpers.Validate<GetPaymentIntentStatusQuery, GetPaymentIntentStatusQueryValidator>(obj, context);
+    }
+}
