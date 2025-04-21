ff8b181c2f4f4f942615f9c2d1dcc4443a6bbcf9
citymaus <citymaus@users.noreply.github.com>
2025-04-21 17:04:02 -0400

    SKS - Added "Camper" roleEnum, more schedule details for Friday and Sunday

--- backend/src/Wedding.Abstractions/Enums/RoleEnum.cs
+++ backend/src/Wedding.Abstractions/Enums/RoleEnum.cs
@@ -11,6 +11,7 @@
         Rehearsal,      // Rehearsal dinner attendees
         Staff,          // Paid help: DJ, Fire spinners
         Manor,          // Residents of manor, invited to breakfasts
+        Camper,         // Guests camping on the grounds
         BetaTester,     // Allows access to screen recordings
         Admin           // Can edit guests and family units
     }
